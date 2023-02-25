e1e5450b03fe2766d81d0c0cc17428a03b81df85
Ankur <yadavankur44@gmail.com>
2023-02-25 22:19:27 +0530

    Home Page almost done.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,68 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>
-        Home
-    </div>
+    <>
+   <section className='home-wrapper-1 py-5'>
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-6">
+            <div className="main-banner position-relative">
+              <img src="images/main-banner-1.jpg" className='img-fluid rounded-3' alt="main banner" />
+              <div className='main-banner-content position-absolute'>
+                <h4>Best Service</h4>
+                <h5>IPhone</h5>
+                <p>Service at your doorstep</p>
+                <Link className='button'>Buy Now</Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <div className='d-flex flex-wrap gap-10 justify-content-between align-items-center'>
+              <div className="small-banner position-relative">
+              <img src="images/catbanner-01.jpg" className='img-fluid rounded-3' alt="main banner" />
+              <div className='small-banner-content position-absolute'>
+                <h4>New Product</h4>
+                <h5>Mobile</h5>
+                <p>Service at your doorstep</p>
+               
+              </div>
+            </div>
+             <div className="small-banner position-relative">
+              <img src="images/catbanner-01.jpg" className='img-fluid rounded-3' alt="main banner" />
+              <div className='small-banner-content position-absolute'>
+                <h4>Book Your service</h4>
+                <h5>Mobile</h5>
+                <p>Service at your doorstep</p>
+               
+              </div>
+            </div>
+            <div className="small-banner position-relative">
+              <img src="images/catbanner-01.jpg" className='img-fluid rounded-3' alt="main banner" />
+              <div className='small-banner-content position-absolute'>
+                <h4>Test your Product</h4>
+                <h5>Mobile</h5>
+                <p>Service at your doorstep</p>
+               
+              </div>
+            </div>
+            <div className="small-banner position-relative">
+              <img src="images/catbanner-01.jpg" className='img-fluid rounded-3' alt="main banner" />
+              <div className='small-banner-content position-absolute'>
+                <h4>Delivery Agent</h4>
+                <h5>Mobile</h5>
+                <p>Service at your doorstep</p>
+               
+              </div>
+            </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+   </section>
+   </>
   )
 }
 
