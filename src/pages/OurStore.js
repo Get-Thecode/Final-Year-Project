b256c150a178d1a56faccb62e5593e79eacd8dc2
Ankur <yadavankur44@gmail.com>
2023-02-27 01:30:27 +0530

    Created Our Store Page

---
+++ src/pages/OurStore.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BreadCrumb from '../Components/BreadCrumb'
+
+const OurStore = () => {
+  return (
+   <>
+        <BreadCrumb title='Our Store'/>
+    </>
+  )
+}
+
+export default OurStore
